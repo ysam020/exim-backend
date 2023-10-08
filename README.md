27b6a7c784de27563a4c6cc9cc6fe5380c519a77
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2023-10-08 22:43:44 +0530

    debugging

--- README.md
+++ README.md
@@ -28,16 +28,19 @@ sudo ./install auto
 ```
 
 Check if CodeDeploy agent is running:
+
 ```
 sudo service codedeploy-agent status
 ```
 
 Location for CodeDeploy logs:
+
 ```
 /opt/codedeploy-agent/deployment-root/deployment-logs/codedeploy-agent-deployments.log
 ```
 
 Uninstall CodeDeploy Agent:
+
 ```
 sudo yum erase codedeploy-agent
 ```
--- app.mjs
+++ app.mjs
@@ -2,34 +2,34 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import compression from "compression";
-import getJobsList from "./routes/getJobList.mjs";
-import getJob from "./routes/getJobRoute.mjs";
-import updateJob from "./routes/updateJob.mjs";
-import addJob from "./routes/addJobsFromExcel.mjs";
-import login from "./routes/login.mjs";
-import register from "./routes/register.mjs";
-import getJobsOverview from "./routes/getJobsOverview.mjs";
-import importerJobs from "./routes/importerJobs.mjs";
-import getImporterList from "./routes/getImporterList.mjs";
-import getUsers from "./routes/getUsers.mjs";
-import getUsersWithJobs from "./routes/getUsersWithJobs.mjs";
-import getAssignedimporter from "./routes/getAssignedImporter.mjs";
-import assignJobs from "./routes/assignJobs.mjs";
-import updateLastJobsDate from "./routes/addLastJobsDate.mjs";
-import getLastJobsDate from "./routes/getLastJobsDate.mjs";
-import importerListToAssignJobs from "./routes/importerListToAssignJobs.mjs";
-import getYears from "./routes/getYears.mjs";
-import removeUser from "./routes/removeUser.mjs";
-import getReportFields from "./routes/getReportFields.mjs";
-import getReport from "./routes/getReport.mjs";
-import convertToExcel from "./routes/convertToExcel.mjs";
-import updateStatus from "./routes/updateStatus.mjs";
-// import sendOtp from "./routes/sendOtp.mjs";
-import sendChangePasswordOtp from "./routes/sendChangePasswordOtp.mjs";
-import feedback from "./routes/feedback.mjs";
-import removeJobs from "./routes/removeJobs.mjs";
-import changePassword from "./routes/changePassword.mjs";
-import downloadReport from "./routes/downloadReport.mjs";
+// import getJobsList from "./routes/getJobList.mjs";
+// import getJob from "./routes/getJobRoute.mjs";
+// import updateJob from "./routes/updateJob.mjs";
+// import addJob from "./routes/addJobsFromExcel.mjs";
+// import login from "./routes/login.mjs";
+// import register from "./routes/register.mjs";
+// import getJobsOverview from "./routes/getJobsOverview.mjs";
+// import importerJobs from "./routes/importerJobs.mjs";
+// import getImporterList from "./routes/getImporterList.mjs";
+// import getUsers from "./routes/getUsers.mjs";
+// import getUsersWithJobs from "./routes/getUsersWithJobs.mjs";
+// import getAssignedimporter from "./routes/getAssignedImporter.mjs";
+// import assignJobs from "./routes/assignJobs.mjs";
+// import updateLastJobsDate from "./routes/addLastJobsDate.mjs";
+// import getLastJobsDate from "./routes/getLastJobsDate.mjs";
+// import importerListToAssignJobs from "./routes/importerListToAssignJobs.mjs";
+// import getYears from "./routes/getYears.mjs";
+// import removeUser from "./routes/removeUser.mjs";
+// import getReportFields from "./routes/getReportFields.mjs";
+// import getReport from "./routes/getReport.mjs";
+// import convertToExcel from "./routes/convertToExcel.mjs";
+// import updateStatus from "./routes/updateStatus.mjs";
+// // import sendOtp from "./routes/sendOtp.mjs";
+// import sendChangePasswordOtp from "./routes/sendChangePasswordOtp.mjs";
+// import feedback from "./routes/feedback.mjs";
+// import removeJobs from "./routes/removeJobs.mjs";
+// import changePassword from "./routes/changePassword.mjs";
+// import downloadReport from "./routes/downloadReport.mjs";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import AWS from "aws-sdk";
@@ -90,61 +90,61 @@ getParameters()
           res.send("Welcome to Exim App");
         });
 
-        app.use(getJobsList);
+        // app.use(getJobsList);
 
-        app.use(getJob);
+        // app.use(getJob);
 
-        app.use(updateJob);
+        // app.use(updateJob);
 
-        app.use(addJob);
+        // app.use(addJob);
 
-        app.use(login);
+        // app.use(login);
 
-        app.use(register);
+        // app.use(register);
 
-        app.use(getJobsOverview);
+        // app.use(getJobsOverview);
 
-        app.use(importerJobs);
+        // app.use(importerJobs);
 
-        app.use(getImporterList);
+        // app.use(getImporterList);
 
-        app.use(getUsers);
+        // app.use(getUsers);
 
-        app.use(getUsersWithJobs);
+        // app.use(getUsersWithJobs);
 
-        app.use(assignJobs);
+        // app.use(assignJobs);
 
-        app.use(getAssignedimporter);
+        // app.use(getAssignedimporter);
 
-        app.use(updateLastJobsDate);
+        // app.use(updateLastJobsDate);
 
-        app.use(getLastJobsDate);
+        // app.use(getLastJobsDate);
 
-        app.use(importerListToAssignJobs);
+        // app.use(importerListToAssignJobs);
 
-        app.use(getYears);
+        // app.use(getYears);
 
-        app.use(getReport);
+        // app.use(getReport);
 
-        app.use(removeUser);
+        // app.use(removeUser);
 
-        app.use(getReportFields);
+        // app.use(getReportFields);
 
-        app.use(convertToExcel);
+        // app.use(convertToExcel);
 
-        app.use(updateStatus);
+        // app.use(updateStatus);
 
-        // app.use(sendOtp);
+        // // app.use(sendOtp);
 
-        app.use(sendChangePasswordOtp);
+        // app.use(sendChangePasswordOtp);
 
-        app.use(feedback);
+        // app.use(feedback);
 
-        app.use(removeJobs);
+        // app.use(removeJobs);
 
-        app.use(changePassword);
+        // app.use(changePassword);
 
-        app.use(downloadReport);
+        // app.use(downloadReport);
 
         app.listen(params["PORT"], () => {
           console.log(`BE started at port ${params["PORT"]}`);
